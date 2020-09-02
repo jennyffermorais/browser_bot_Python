091e1d8dad981326870e4d3c30d158c15198d64a
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-09-02 18:02:40 -0300

    descomentar o código

--- browser_bot.py
+++ browser_bot.py
@@ -70,12 +70,12 @@ sleep(10)
 browser.switch_to.window(window_after) #mudando de janela
 
 # Cria e escreve no arquivo txt
-# body = browser.find_element_by_tag_name('body')
-# file_text = open('texto_vw_com_br.txt', 'w')
-# file_text.writelines(body.text)
-# file_text.close()
+body = browser.find_element_by_tag_name('body')
+file_text = open('texto_vw_com_br.txt', 'w')
+file_text.writelines(body.text)
+file_text.close()
 
-# sleep(5) 
+sleep(5) 
 
 # Salva a página em pdf
 
@@ -96,10 +96,10 @@ sleep(3)
 page = browser.find_element_by_tag_name('body') 
 page.screenshot('scrape.png') #salva a página como png
 
-# sleep(1)
-# print_page = Image.open(r'scrape.png')
-# pdf = print_page.convert('RGB')
-# pdf.save(r'pdf_vw_com_br.pdf') #converte a imagem em pdf
+sleep(1)
+print_page = Image.open(r'scrape.png')
+pdf = print_page.convert('RGB')
+pdf.save(r'pdf_vw_com_br.pdf') #converte a imagem em pdf
 
 # Pegando todos os links do site
 req = requests.get(browser.current_url)
